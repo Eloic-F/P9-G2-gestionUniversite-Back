a0f79e170c61720f66ff2daf4b690eb80045dce3
Hamada976 <hamada.fayadhu@gmail.com>
2022-09-06 11:37:36 +0200

    ajout classe compte

--- src/main/java/com/inti/entities/Compte.java
+++ src/main/java/com/inti/entities/Compte.java
@@ -4,6 +4,8 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
 
 @Entity
 public class Compte {
@@ -12,5 +14,56 @@ public class Compte {
 	private Long idCompte;
 	private Long numeroCompte;
 	private String statut;
+	@OneToOne
+	@JoinColumn(name="id_personne")
+	private Personne personne;
+	
+	public Compte() {
+		
+	}
+
+	public Compte(Long idCompte, Long numeroCompte, String statut, Personne personne) {
+
+		this.idCompte = idCompte;
+		this.numeroCompte = numeroCompte;
+		this.statut = statut;
+		this.personne = personne;
+	}
+
+	public Long getIdCompte() {
+		return idCompte;
+	}
+
+	public void setIdCompte(Long idCompte) {
+		this.idCompte = idCompte;
+	}
+
+	public Long getNumeroCompte() {
+		return numeroCompte;
+	}
+
+	public void setNumeroCompte(Long numeroCompte) {
+		this.numeroCompte = numeroCompte;
+	}
+
+	public String getStatut() {
+		return statut;
+	}
+
+	public void setStatut(String statut) {
+		this.statut = statut;
+	}
+
+	public Personne getPersonne() {
+		return personne;
+	}
+
+	public void setPersonne(Personne personne) {
+		this.personne = personne;
+	}
+	
+	
+	
+	
 
 }
