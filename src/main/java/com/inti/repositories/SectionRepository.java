6e5accdb2ca3646c07dc74f2a3e6bdc228618f6c
DylanMerlateau <dylan.merlateau@gmail.com>
2022-09-06 16:27:41 +0200

    ajout Annotation repository

--- src/main/java/com/inti/repositories/SectionRepository.java
+++ src/main/java/com/inti/repositories/SectionRepository.java
@@ -1,9 +1,11 @@
 package com.inti.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.inti.entities.Section;
 
+@Repository
 public interface SectionRepository extends JpaRepository<Section,Long>{
 
 }
--- src/main/java/com/inti/repositories/UniversiteRepository.java
+++ src/main/java/com/inti/repositories/UniversiteRepository.java
@@ -1,9 +1,11 @@
 package com.inti.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.inti.entities.Universite;
 
+@Repository
 public interface UniversiteRepository extends JpaRepository<Universite,Long> {
 
 }
