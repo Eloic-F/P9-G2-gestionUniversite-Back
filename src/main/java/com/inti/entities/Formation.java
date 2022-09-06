151592fe46044bfdc21c963a9034bd2c384419df
DylanMerlateau <dylan.merlateau@gmail.com>
2022-09-06 11:23:29 +0200

    Modifs classes Univ,Section,Formation,CentreDeRecherche,Academie

--- src/main/java/com/inti/entities/Formation.java
+++ src/main/java/com/inti/entities/Formation.java
@@ -1,5 +1,90 @@
 package com.inti.entities;
 
+import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
+@Entity
 public class Formation {
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private Long idFormation;
+	private String titreFormation;
+	private String gradeFormation;
+	
+	@OneToMany(mappedBy = "Formation")
+	List<Cours> courss;
+	
+	@OneToMany(mappedBy = "Formation")
+	List<Classe> classes;
+
+	public Formation() {
+		super();
+	}
+
+	public Formation(Long idFormation, String titreFormation, String gradeFormation, List<Cours> courss,
+			List<Classe> classes) {
+		super();
+		this.idFormation = idFormation;
+		this.titreFormation = titreFormation;
+		this.gradeFormation = gradeFormation;
+		this.courss = courss;
+		this.classes = classes;
+	}
+
+	public Long getIdFormation() {
+		return idFormation;
+	}
+
+	public void setIdFormation(Long idFormation) {
+		this.idFormation = idFormation;
+	}
+
+	public String getTitreFormation() {
+		return titreFormation;
+	}
+
+	public void setTitreFormation(String titreFormation) {
+		this.titreFormation = titreFormation;
+	}
+
+	public String getGradeFormation() {
+		return gradeFormation;
+	}
+
+	public void setGradeFormation(String gradeFormation) {
+		this.gradeFormation = gradeFormation;
+	}
+
+	public List<Cours> getCourss() {
+		return courss;
+	}
+
+	public void setCourss(List<Cours> courss) {
+		this.courss = courss;
+	}
+
+	public List<Classe> getClasses() {
+		return classes;
+	}
+
+	public void setClasses(List<Classe> classes) {
+		this.classes = classes;
+	}
+
+	@Override
+	public String toString() {
+		return "Formation [idFormation=" + idFormation + ", titreFormation=" + titreFormation + ", gradeFormation="
+				+ gradeFormation + ", courss=" + courss + ", classes=" + classes + "]";
+	}
+
+	
+	
+	
 
 }
--- src/main/java/com/inti/entities/Section.java
+++ src/main/java/com/inti/entities/Section.java
@@ -25,4 +25,56 @@ public class Section {
 	@OneToMany(mappedBy = "Section")
 	List<Formation> formations;
 
+	public Section() {
+		super();
+	}
+
+	public Section(Long idSection, String nomSection, Universite universite, List<Formation> formations) {
+		super();
+		this.idSection = idSection;
+		this.nomSection = nomSection;
+		this.universite = universite;
+		this.formations = formations;
+	}
+
+	public Long getIdSection() {
+		return idSection;
+	}
+
+	public void setIdSection(Long idSection) {
+		this.idSection = idSection;
+	}
+
+	public String getNomSection() {
+		return nomSection;
+	}
+
+	public void setNomSection(String nomSection) {
+		this.nomSection = nomSection;
+	}
+
+	public Universite getUniversite() {
+		return universite;
+	}
+
+	public void setUniversite(Universite universite) {
+		this.universite = universite;
+	}
+
+	public List<Formation> getFormations() {
+		return formations;
+	}
+
+	public void setFormations(List<Formation> formations) {
+		this.formations = formations;
+	}
+
+	@Override
+	public String toString() {
+		return "Section [idSection=" + idSection + ", nomSection=" + nomSection + ", universite=" + universite
+				+ ", formations=" + formations + "]";
+	}
+	
+	
+
 }
