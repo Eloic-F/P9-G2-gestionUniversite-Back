c8a8ebfad99c1fd1ed6b11b851563e490b88ab6e
Eloïc F <eloic.ferdinand@gmail.com>
2022-09-06 11:10:22 +0200

    model adresse

--- src/main/java/com/inti/model/Adresse.java
+++ src/main/java/com/inti/model/Adresse.java
@@ -1,5 +1,45 @@
 package com.inti.model;
 
+import javax.persistence.Embeddable;
+
+@Embeddable
 public class Adresse {
+	private String rue;
+	private Long codePostal;
+	private String ville;
+
+	public Adresse() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public Adresse(String rue, Long codePostal, String ville) {
+		this.rue = rue;
+		this.codePostal = codePostal;
+		this.ville = ville;
+	}
+
+	public String getRue() {
+		return rue;
+	}
+
+	public void setRue(String rue) {
+		this.rue = rue;
+	}
+
+	public Long getCodePostal() {
+		return codePostal;
+	}
+
+	public void setCodePostal(Long codePostal) {
+		this.codePostal = codePostal;
+	}
+
+	public String getVille() {
+		return ville;
+	}
+
+	public void setVille(String ville) {
+		this.ville = ville;
+	}
 
 }
