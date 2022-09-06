1e31f7359e49f46e5773a50a050a4f7d11f7a1db
Eloïc F <eloic.ferdinand@gmail.com>
2022-09-06 12:08:24 +0200

    Interface service personne

--- src/main/java/com/inti/service/interfaces/IPersonneService.java
+++ src/main/java/com/inti/service/interfaces/IPersonneService.java
@@ -1,5 +1,16 @@
 package com.inti.service.interfaces;
 
+import java.util.List;
+
+import com.inti.entities.Personne;
+
 public interface IPersonneService {
+	List<Personne> findAll();
+
+	Personne findOne(Long id);
+
+	Personne save(Personne personne);
+
+	void delete(Long id);
 
 }
